f6dc8d8da8c21f33bf4d3fdd226c677b66083676
julian flood and monique st laurent <monique@>
2017-04-03 20:18:55 -0700

    add index forms and some books

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,21 @@ var library = [
   fantasy3 = new Book('Northern Lights', 'Philip Pullman', 'fantasy', 1995, 432),
   fantasy4 = new Book('The Voyage of the Dawn Treader', 'C.S. Lewis', 'fantasy',1952, 256),
   fantasy5 = new Book('American Gods', 'Neil Gaiman', 'fantasy', 2001, 784),
+  romance1 = new Book("Pride and Prejudice","Jane Austin","romance",1813,656)
+  romance2 = new Book("Twilight","Stephanie Meyer","romance",2006,544)
+  romance3 = new Book("The Truth About Forever", "Sarah Dessen", "romance",2004,374)
+  romance4 = new Book("Gone with the Wind", "Margaret Mitchell", "romance",1999,1037)
+  romance5 = new Book("Crave(The Hot Addiction #1)","Joya Ryan", "romance",2015,134)
+  nonfiction1 = new Book("In Cold Blood", "Truman Capote", "nonfiction",1966,343)
+  nonfiction2 = new Book("Fast Food Nation", "Eric Schlosser",2001, 384)
+  nonfiction3 = new Book("Nikola Tesla", "Sean Patrick",2013,46)
+  nonfiction4 = new Book("Into the Wild","Jon Krakauer",1995,224)
+  nonfiction5 = new Book("Silent Spring", "Rachel Carson",1962,400)
+}
+
 ];
+
+
 
 function Book(bookTitle, author, genre, year, about, pageCount) {
   this.bookTitle = bookTitle;
